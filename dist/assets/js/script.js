f020b8df749d43f38981a87797bd7d47c9ebc0a1
Kevin <kevindelaunay74@gmail.com>
2022-02-28 14:17:36 +0100

    filter upgrade

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -5,7 +5,21 @@
 /*=======================| Init Variable =*/
 
 let divs = document.getElementsByClassName("div-photo");
+let filter = document.getElementsByClassName("li-filter");
 let divTab = Array.from(divs);
+let filterTab = Array.from(filter);
+
+
+
+console.log("mon tableau :" + filterTab.className);
+console.log("mon tableau 2 :" + divTab);
+
+function addfilter (){
+    for (let a=0 ; a < filterTab.length ; a++){
+     console.log(filterTab[a].className)
+    }
+}
+addfilter();
 
 /*=======================| Add Function =*/
 
