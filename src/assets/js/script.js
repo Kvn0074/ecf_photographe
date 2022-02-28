f4711c5a8f7a4cb8329e2136649a540f02300d41
Kevin <kevindelaunay74@gmail.com>
2022-02-28 17:04:21 +0100

    Beautify code and pagination

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,14 +2,17 @@
 /*====== JavaScript ======*/
 /*========================*/
 
-/*=======================| Init Variable =*/
+
+/*==========================================*/
+/*============| PHOTO FILTER |==============*/
+/*==========================================*/
+
+    /*==========| variables =*/
 
 let divs = document.getElementsByClassName("div-photo");
 let divTab = Array.from(divs);
 
-
-
-/*=======================| Add Function =*/
+    /*==========| functions =*/
 
 function applyfilter(category){
     for (let i=0; i < divTab.length ; i++){
@@ -30,17 +33,30 @@ function applyfilter(category){
     }
 }
 
-/*========================*/
+/*==========================================*/
+/*============| SCROLL DIV |================*/
+/*==========================================*/
 
-/*=======================| Add Function =*/
-const ratio = 0.5;
+    /*===================== Use the Intersection Observer API =====================*/
+    /*== https://developer.mozilla.org/fr/docs/Web/API/Intersection_Observer_API ==*/
+    /*=============================================================================*/
+
+    /*==========| variables =*/
+
+let ratio = 0.5; /*___ = 50% of visibilty for the element ___*/
+let hideDiv = document.querySelectorAll('.hide');
+
+
+    /*==========| Intersection Observer's options =*/
+
 const options = {
-    root: null,  /*= zone d'affichage =*/
+    root: null,  
     rootMargin: '0px',
-    threshold: ratio /*= detection intersection si = 1, tout l'ellement doit etre visible =*/
+    threshold: ratio 
   }
 
-let hideDiv = document.querySelectorAll('.hide');
+
+
 
 function getInter (entries, observer){
     entries.forEach(function (entry){
@@ -51,11 +67,21 @@ function getInter (entries, observer){
     })
     
 }
-  
-const observer = new IntersectionObserver(getInter, options);
+
+    /*==========| Creation of IntersectionObserver  =*/
+
+    const observer = new IntersectionObserver(getInter, options);
+
+    /*==========| Observation declaration for each '.hide' =*/
+
 hideDiv.forEach(function (appear){
     observer.observe(appear)
 })
+
+/*========================*/
+/*========================*/
+/*========================*/
+
 
 
 
